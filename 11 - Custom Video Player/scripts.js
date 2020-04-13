9e8d2731a6a35f4a9733981217f3b1b1de59334e
Eloisa Borromeo <27989510+eefborromeo@users.noreply.github.com>
2020-04-13 15:28:42 +0800

    selected playback control elements

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,10 @@
+// Select Elements
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress');
+const toggle = document.querySelector('.toggle');
+const volume = document.querySelector('input[name="volume"]');
+const playbackRate = document.querySelector('input[name="playbackRate"]');
+const playbackButtons = document.querySelectorAll('button[data-skip]');
+
+// Create Functons
+// Attach Event Listeners
