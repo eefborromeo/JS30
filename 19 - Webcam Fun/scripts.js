66b9d3837ef4df05e8c32f3bdb0458d92cc68ca0
Eloisa Borromeo <27989510+eefborromeo@users.noreply.github.com>
2020-04-21 14:39:16 +0800

    get webcam access an display to video

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,21 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices
+    // get access to webcam not audio
+    .getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      // if approved, then set localMediaStream to video's srcObject
+      video.srcObject = localMediaStream;
+      // play video for continuous stream
+      video.play();
+    })
+    // create catch error if webcam access was denied
+    .catch(err => {
+      console.error('Webcam needed', err);
+    });
+}
+
+getVideo();
