23503bbd8a958088524812df3a8c2de82411191c
Eloisa Borromeo <27989510+eefborromeo@users.noreply.github.com>
2020-04-29 16:58:58 +0800

    create and display displayTimeLeft()

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -1,9 +1,22 @@
+const timeLeft = document.querySelector('.display__time-left');
+
 function timer(seconds) {
+  displayTimeLeft(seconds);
   const countdown = setInterval(function() {
     seconds -= 1;
-    console.log(seconds);
     if (seconds === 0) {
       clearInterval(countdown);
     }
+    displayTimeLeft(seconds);
   }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const countdown = `${minutes}:${
+    remainderSeconds < 10 ? '0' : ''
+  }${remainderSeconds}`;
+  timeLeft.textContent = countdown;
+  document.title = countdown;
 }
