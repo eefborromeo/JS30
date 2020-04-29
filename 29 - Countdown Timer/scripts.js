f88e7b417939c087e1fe57687049574b9be5bef6
Eloisa Borromeo <27989510+eefborromeo@users.noreply.github.com>
2020-04-29 17:32:52 +0800

    get and display displayEndTime()

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -1,7 +1,13 @@
 const timeLeft = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
+  // get time now
+  const now = Date.now();
+  //   modify new time with added seconds
+  const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
   const countdown = setInterval(function() {
     seconds -= 1;
     if (seconds === 0) {
@@ -17,6 +23,18 @@ function displayTimeLeft(seconds) {
   const countdown = `${minutes}:${
     remainderSeconds < 10 ? '0' : ''
   }${remainderSeconds}`;
+  //   display countdown
   timeLeft.textContent = countdown;
   document.title = countdown;
+}
+
+function displayEndTime(timestamp) {
+  // get current time
+  const timeEnd = new Date(timestamp);
+  const hours = timeEnd.getHours();
+  const minutes = timeEnd.getMinutes();
+  //   display time to be back
+  endTime.textContent = `Come back at ${hours > 12 ? hours - 12 : hours}:${
+    minutes < 10 ? '0' : ''
+  }${minutes}`;
 }
